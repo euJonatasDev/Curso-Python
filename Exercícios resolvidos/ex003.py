a1ef8d6dc2ca3f9fc2e9e8c19c18798e05bd2dc4
Jônatas Pereira <jonataspereirarocha@gmail.com>
2022-08-24 09:53:11 -0300

    Update ex003.py

---
+++ Exercícios resolvidos/ex003.py
@@ -0,0 +1,5 @@
+# Enunciado: Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+print('A soma entre o primeiro número e o segundo é {} ' .format(num1 + num2))

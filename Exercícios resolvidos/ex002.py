5f6738c5889ce240e668f50b5c78aed348a822fe
Jônatas Pereira <jonataspereirarocha@gmail.com>
2022-08-24 09:43:20 -0300

    Update ex002.py

---
+++ Exercícios resolvidos/ex002.py
@@ -0,0 +1,4 @@
+# Enunciado: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Digite o seu nome: ')
+print('Prazer em te conhecer {}' .format(nome))
